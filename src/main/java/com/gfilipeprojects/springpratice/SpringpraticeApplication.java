d560fcc62f009725d13d6b6cf17191a87b79c2b5
g-filipe <galvaofilipe.adv@gmail.com>
2023-05-06 13:20:50 -0300

    project created. First Entitie and Resource class created

---
+++ src/main/java/com/gfilipeprojects/springpratice/SpringpraticeApplication.java
@@ -0,0 +1,13 @@
+package com.gfilipeprojects.springpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpraticeApplication.class, args);
+	}
+
+}
